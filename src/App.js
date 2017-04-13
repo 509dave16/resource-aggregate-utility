cbda3ec3a85100dad740262e8268c07c032261de
David Fall <davidfall@rocketmail.com>
2017-04-12 18:48:41 -0700

    App Husk: Tested! - We have a very barebones app husk that has some leftovers from the `tic-tac-toe-react-firebase`

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import styles from './App.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div> Hello World! Boom!! Boom Again!!!</div>,
+  document.getElementById('app')
+);
