81e470cb4a39d616f95b53a7730ab4f079fe6a13
David Fall <davidfall@rocketmail.com>
2017-04-13 14:11:41 -0700

    Changed the code locale of I/O data for testing to be in a comman place

---
+++ src/Common/Data/chartlytics-js-test-output.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    "type": "Person",
+    "order": 1,
+    "people": [
+      "Brian"
+    ]
+  },
+  {
+    "type": "Place",
+    "order": 2,
+    "name": "Ohio"
+  },
+  {
+    "type": "Place",
+    "order": 12,
+    "name": "Ohio"
+  },
+  {
+    "type": "Person",
+    "order": 19,
+    "people": [
+      "Sarah",
+      "Eric"
+    ]
+  },
+  {
+    "type": "Place",
+    "order": 20,
+    "name": "Home"
+  },
+  {
+    "type": "Person",
+    "order": 199,
+    "people": [
+      "Sam"
+    ]
+  }
+];
--- src/Utilities/ResourceAggregation/__tests__/ResourceAggregationUtility.test.js
+++ src/Utilities/ResourceAggregation/__tests__/ResourceAggregationUtility.test.js
@@ -1,83 +1,16 @@
 /**
- * @file Tests for the Resource Aggregation Utility. Coverage for the following functions:
+ * @file Jest Tests for the Resource Aggregation Utility. Coverage for the following functions:
  *  1. aggregateAdjacentResources
  * @see {@link /Utilities/ResourceAggregation/ResourceAggregationUtility.js#AggregateAdjacentResources}
  */
 
 import { aggregateAdjacentResources } from '../ResourceAggregationUtility.js';
 import strategy from '../Strategies/AggregateAdjacentResources/PersonStrategy.js';
+import input from '/Common/Data/chartlytics-js-test-input.js';
+import output from '/Common/Data/chartlytics-js-test-output.js';
 
 describe('aggregateAdjacentResources', () => {
-  const inputResources = [{
-    type: 'Person',
-    order: 1,
-    name: 'Brian',
-  }, {
-    type: 'Place',
-    order: 2,
-    name: 'Ohio',
-  }, {
-    type: 'Place',
-    order: 12,
-    name: 'Ohio',
-  }, {
-    type: 'Person',
-    order: 14,
-    name: 'Sarah',
-  },  {
-    type: 'Person',
-    order: 199,
-    name: 'Sam',
-  }, {
-    type: 'Person',
-    order: 19,
-    name: 'Eric',
-  }, {
-    type: 'Place',
-    order: 20,
-    name: 'Home',
-  }];
-
-  const output = [
-    {
-      "type": "Person",
-      "order": 1,
-      "people": [
-        "Brian"
-      ]
-    },
-    {
-      "type": "Place",
-      "order": 2,
-      "name": "Ohio"
-    },
-    {
-      "type": "Place",
-      "order": 12,
-      "name": "Ohio"
-    },
-    {
-      "type": "Person",
-      "order": 19,
-      "people": [
-        "Sarah",
-        "Eric"
-      ]
-    },
-    {
-      "type": "Place",
-      "order": 20,
-      "name": "Home"
-    },
-    {
-      "type": "Person",
-      "order": 199,
-      "people": [
-        "Sam"
-      ]
-    }
-  ];
-  test('the almighty test :) !!!', () => {
-    expect(aggregateAdjacentResources(inputResources, strategy)).toEqual(output);
+  test('chartlytics js test', () => {
+    expect(aggregateAdjacentResources(input, strategy)).toEqual(output);
   });
 });
